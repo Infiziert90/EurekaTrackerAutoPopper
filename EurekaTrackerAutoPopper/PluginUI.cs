5fb030bbf0f1977fba0f2713253e0e5240a110b3
Infi <infiziert@protonmail.ch>
2023-07-14 19:47:37 +0200

    Add KoFI

--- EurekaTrackerAutoPopper/PluginUI.cs
+++ EurekaTrackerAutoPopper/PluginUI.cs
@@ -710,6 +710,13 @@ namespace EurekaTrackerAutoPopper
                     if (ImGui.Button(Loc.Localize("Config Button - Github Issues", "Issues")))
                         Dalamud.Utility.Util.OpenLink("https://github.com/Infiziert90/EurekaTrackerAutoPopper/issues");
                     ImGui.PopStyleColor();
+
+                    ImGui.SameLine();
+
+                    ImGui.PushStyleColor(ImGuiCol.Button, new Vector4(0.12549f, 0.74902f, 0.33333f, 0.6f));
+                    if (ImGui.Button("Ko-Fi Tip"))
+                        Dalamud.Utility.Util.OpenLink("https://ko-fi.com/infiii");
+                    ImGui.PopStyleColor();
                 }
                 ImGui.EndChild();
 
