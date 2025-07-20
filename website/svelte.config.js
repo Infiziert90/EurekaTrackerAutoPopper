1e2c4d8539cbca3ded877643f3ce130c170b237a
krz <maxlav78@gmail.com>
2025-07-21 00:07:50 +0200

    Setup GitHub Pages deployment

--- website/svelte.config.js
+++ website/svelte.config.js
@@ -1,6 +1,18 @@
 import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = { kit: { adapter: adapter() }, compilerOptions: { runes: true } };
+const config = {
+    kit: {
+        adapter: adapter({
+            fallback: '404.html'
+        }),
+        paths: {
+            base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+        }
+    },
+    compilerOptions: { 
+        runes: true
+    }
+};
 
 export default config;
