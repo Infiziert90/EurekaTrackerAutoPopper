ef1e42edb97cd4e2d9cecb200afbf8f6c5cc84be
Infi <infiziert@protonmail.ch>
2025-09-03 23:33:55 +0200

    Another base url fix

--- website/svelte.config.js
+++ website/svelte.config.js
@@ -7,10 +7,10 @@ const config = {
             fallback: '404.html'
         }),
         paths: {
-            base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+            base: process.env.NODE_ENV === 'production' ? '' : '',
         }
     },
-    compilerOptions: { 
+    compilerOptions: {
         runes: true
     }
 };
